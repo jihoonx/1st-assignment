beeb38e4fc10f16b961a5cbe02ace96370397af1
jihoonx <31638889+jihoonx@users.noreply.github.com>
2017-09-05 11:40:20 +0900

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+
+public class Assignment {
+	public static void main(String[] args) {
+		System.out.println("2012314037"); // 학번
+		System.out.println("최지훈"); // 이름
+		System.out.println("010 7499 2150"); // 연락처
+		System.out.println("jihoonx"); // 깃허브 주소
+	}
+}
